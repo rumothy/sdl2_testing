65a9fd4f7d9084bd7aece13663be5848c2ad926d
rumothy <rumothy@yahoo.com>
2013-11-09 21:25:01 -0500

    SDL2.0 Testing initial check in

---
+++ sdl2_testing/main.cpp
@@ -0,0 +1,31 @@
+#include <Windows.h>
+#include <iostream>
+#include "SDL.h"
+
+SDL_Window* g_pWindow = 0;
+SDL_Renderer* g_pRenderer = 0;
+int wmain(int argc, char* args[])
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+		g_pWindow = SDL_CreateWindow("Chapter 1: Setting up SDL",
+			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+			640, 480,
+			SDL_WINDOW_SHOWN);
+		if (g_pWindow != 0)
+		{
+			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+		}
+	}
+	else
+	{
+		return 1; 
+	}
+
+	SDL_SetRenderDrawColor(g_pRenderer, 127, 0, 0, 255);
+	SDL_RenderClear(g_pRenderer);
+	SDL_RenderPresent(g_pRenderer);
+	SDL_Delay(10000);
+	SDL_Quit();
+	return 0;
+}
